9d8ba0c83862f284ea0289fb397caa9f4637cf83
Neeraj Auchar <neerajauchar@gmail.com>
2023-07-07 19:09:44 +0530

    final done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Body from './components/Body/Body';
 
 function App() {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
-import React from 'react'
 import styles from './Header.module.css'
 import resumesvg from '../assets/resume.svg'
+
 
 function Header() {
     return (
